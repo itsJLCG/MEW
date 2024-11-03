80e67feb8569b824c4cb21aa3b095dc79bab3040
Jhon Ludwig Gayapa <jhonludwig.gayapa@tup.edu.ph>
2024-11-03 15:26:53 +0800

    Updated Product with Number Valid

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sidebarReducer from "./slices/sidebarSlice";
+/* import brandReducer from './slices/brandSlice'; */
 
 export const store = configureStore({
   reducer: {
     sidebar: sidebarReducer,
+/*     brands: brandReducer, */
   },
 });
+
