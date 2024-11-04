e94386b91625ac93182b08ee4e0d91a81ee67ae0
Jhon Ludwig Gayapa <jhonludwig.gayapa@tup.edu.ph>
2024-11-05 00:22:57 +0800

    Remove brand slice store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sidebarReducer from "./slices/sidebarSlice";
-/* import brandReducer from './slices/brandSlice'; */
 
 export const store = configureStore({
   reducer: {
     sidebar: sidebarReducer,
-/*     brands: brandReducer, */
   },
 });
 
